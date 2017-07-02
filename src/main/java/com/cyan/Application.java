dfe60cf0f22c31ff865f7512f6834b8335cb0e2f
Calvin Zhou <Calvin@MacBook.local>
2017-07-02 15:37:15 +0800

    First release

---
+++ src/main/java/com/cyan/Application.java
@@ -0,0 +1,33 @@
+package com.cyan;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer {
+	
+	private static Logger logger = LogManager.getLogger();
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		logger.debug("=====================First Intial");
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value="/f")
+	public String first() {
+		logger.debug("=====================Somebody hit me");
+		return "First Spring Boot";
+	}
+}
